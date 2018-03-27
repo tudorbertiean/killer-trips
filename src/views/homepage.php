0029ce0cdb1561d792302d6c28495c749be79fc1
Tudor Bertiean <tudorbertiean@hotmail.com>
2018-03-27 15:33:41 -0400

    thumbs down n up

--- src/views/homepage.php
+++ src/views/homepage.php
@@ -9,20 +9,24 @@
       <nav class="navbar navbar-default">
         <div class="container-fluid">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-              <img src="../images/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
-              Killer Trips
-            </a>          
+            <a class="navbar-brand" href="#">Killer Trips</a>
           </div>
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Page 1</a></li>
-            <li><a href="#">Page 2</a></li>
-            <li><a href="#">Page 3</a></li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://localhost:8080/killer-trips/src/views/register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="http://localhost:8080/killer-trips/src/views/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+          <form class="navbar-form navbar-left" action="/action_page.php">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Search">
+              <div class="input-group-btn">
+                <button class="btn btn-default" type="submit">
+                  <i class="glyphicon glyphicon-search"></i>
+                </button>
+              </div>
+            </div>
           </form>
         </div>
       </nav>
@@ -36,31 +40,37 @@
               <div class="col-6 col-lg-4">
                 <h2>City 1</h2>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p style="color:green">167 <span class="glyphicon glyphicon-thumbs-up"></span></p>
                 <p><a class="btn btn-secondary" href="#" role="button">See more &raquo;</a></p>
               </div><!--/span-->
               <div class="col-6 col-lg-4">
-                <h2>City 2</h2>
+                <h2>City 2</h2> 
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p style="color:red">-120 <span class="glyphicon glyphicon-thumbs-down"></span></p>
                 <p><a class="btn btn-secondary" href="#" role="button">See more &raquo;</a></p>
               </div><!--/span-->
               <div class="col-6 col-lg-4">
                 <h2>City 3</h2>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p style="color:green">27 <span class="glyphicon glyphicon-thumbs-up"></span></p>
                 <p><a class="btn btn-secondary" href="#" role="button">See more &raquo;</a></p>
               </div><!--/span-->
               <div class="col-6 col-lg-4">
                 <h2>City 4</h2>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p style="color:green">0 <span class="glyphicon glyphicon-thumbs-up"></span></p>
                 <p><a class="btn btn-secondary" href="#" role="button">See more &raquo;</a></p>
               </div><!--/span-->
               <div class="col-6 col-lg-4">
                 <h2>City 5</h2>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p style="color:red">-100 <span class="glyphicon glyphicon-thumbs-down"></span></p>
                 <p><a class="btn btn-secondary" href="#" role="button">See more &raquo;</a></p>
               </div><!--/span-->
               <div class="col-6 col-lg-4">
                 <h2>City 6</h2>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p style="color:green">5 <span class="glyphicon glyphicon-thumbs-up"></span></p>
                 <p><a class="btn btn-secondary" href="#" role="button">See more &raquo;</a></p>
               </div><!--/span-->
             </div><!--/row-->
