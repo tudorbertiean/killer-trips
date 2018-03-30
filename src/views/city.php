bdffb26bab7958d685009ed09f477bb142468cd8
Tudor Bertiean <tudorbertiean@hotmail.com>
2018-03-29 22:01:19 -0400

    cities attractions

--- src/views/city.php
+++ src/views/city.php
@@ -82,6 +82,41 @@
             
             <h2>Attractions:</h2>
 
+            <div id="attractionCarousel" class="carousel slide" data-interval="false">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+              </ol>
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner">
+                <div class="item active">
+                  <img src="../images/toronto.jpg">
+                  <div class="carousel-caption">
+                    <h3>Toronto</h3>
+                    <p>Blah blah blah</p>
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="../images/vancouver.jpg">
+                  <div class="carousel-caption">
+                    <h3>Vancouver</h3>
+                    <p>Blah blah blah</p>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Left and right controls -->
+              <a class="left carousel-control" href="#attractionCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#attractionCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+
             <h2>Things That May Kill You:</h2>
 
             <h2>Comments:</h2>
