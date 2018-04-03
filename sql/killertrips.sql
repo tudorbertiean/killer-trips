15a800b3c335344d4151d14e38df77a9fdedf6b0
Tudor Bertiean <tudorbertiean@hotmail.com>
2018-04-02 22:33:13 -0400

    sql

--- sql/killertrips.sql
+++ sql/killertrips.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 30, 2018 at 03:59 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost
+-- Generation Time: Apr 03, 2018 at 04:32 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,91 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `killertrips`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `attractions`
---
-
-CREATE TABLE `attractions` (
-  `attractionid` int(11) NOT NULL,
-  `image` varchar(200) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `description` varchar(1000) NOT NULL,
-  `cityid` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cities`
---
-
-CREATE TABLE `cities` (
-  `cityid` int(11) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `description` varchar(1000) NOT NULL,
-  `country` varchar(100) NOT NULL,
-  `image` varchar(200) NOT NULL,
-  `votescore` int(11) NOT NULL DEFAULT '0',
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `cities`
---
-
-INSERT INTO `cities` (`cityid`, `city`, `description`, `country`, `image`, `votescore`, `date`) VALUES
-(1, 'Toronto', 'A global city, Toronto is a centre of business, finance, arts, and culture, and is recognized as one of the most multicultural and cosmopolitan cities in the world. ... York was renamed and incorporated as the city of Toronto in 1834, and became the capital of the province of Ontario during Canadian Confederation in 1867.', 'Canada', 'toronto.jpg', 0, '2018-03-27 20:31:17'),
-(2, 'Vancouver', 'The City of Vancouver is a coastal, seaport city on the mainland of British Columbia. Located on the western half of the Burrard Peninsula, Vancouver is bounded to the north by English Bay and the Burrard Inlet and to the south by the Fraser River.', 'Canada', 'vancouver.jpg', 0, '2018-03-27 20:31:49'),
-(4, 'Tokyo', 'Tokyo is Japan\'s capital and the world\'s most populous metropolis. It is also one of Japan\'s 47 prefectures, consisting of 23 central city wards and multiple cities, towns and villages west of the city center. The Izu and Ogasawara Islands are also part of Tokyo.', 'Japan', 'tokyo.jpg', 0, '2018-03-27 21:32:34'),
-(5, 'Cancun', 'Cancun is located in Quintana Roo, where Mayan culture shines on every corner. Surrounded by the breathtaking Caribbean Sea, soft coral sands, and the striking shade of blue that emerges from its crystal-clear waters, a visit to Cancun will make you wonder whether you are in a dream or else.', 'Mexico', 'cancun.jpg', 0, '2018-03-27 21:35:15');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
-
-CREATE TABLE `comments` (
-  `commentid` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `userid` int(11) NOT NULL,
-  `comment` varchar(400) NOT NULL,
-  `cityid` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` (`commentid`, `date`, `userid`, `comment`, `cityid`) VALUES
-(1, '2018-03-28 00:22:16', 1, 'I love Vancouver!!!!!!', 2),
-(2, '2018-03-28 00:22:54', 1, 'I know a friend who recently got killed here by lightning.. How unfortunate.', 2),
-(3, '2018-03-28 02:55:21', 1, 'Test', 1),
-(4, '2018-03-28 03:47:52', 1, 'Test', 2),
-(5, '2018-03-28 03:50:54', 1, 'What is wrong with this dumb city', 2),
-(6, '2018-03-28 04:23:04', 1, 'Vancouver ruined my life', 2),
-(7, '2018-03-28 19:10:56', 1, 'I was born here and died on the same day', 1),
-(8, '2018-03-28 19:50:30', 12, 'Azn central', 4),
-(9, '2018-03-28 22:57:50', 12, 'bitch', 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `killinfo`
---
-
-CREATE TABLE `killinfo` (
-  `killid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `killtext` text NOT NULL,
-  `votescore` int(11) NOT NULL DEFAULT '0',
-  `killname` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -124,52 +41,13 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`userid`, `username`, `password`, `permission`) VALUES
 (1, 'tudorbertiean', '33d890d33f91d52fc9b405a0dda65336', 'admin'),
-(12, 'jackstufs', '33d890d33f91d52fc9b405a0dda65336', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vote`
---
-
-CREATE TABLE `vote` (
-  `voteid` int(11) NOT NULL,
-  `cityid` int(11) NOT NULL,
-  `commentid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(12, 'jackstufs', '33d890d33f91d52fc9b405a0dda65336', 'admin'),
+(13, 'tud', '33d890d33f91d52fc9b405a0dda65336', 'admin'),
+(14, 'tttt', '33d890d33f91d52fc9b405a0dda65336', 'admin');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `attractions`
---
-ALTER TABLE `attractions`
-  ADD PRIMARY KEY (`attractionid`),
-  ADD KEY `cityid` (`cityid`);
-
---
--- Indexes for table `cities`
---
-ALTER TABLE `cities`
-  ADD PRIMARY KEY (`cityid`);
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`commentid`),
-  ADD KEY `userid` (`userid`),
-  ADD KEY `cityid` (`cityid`);
-
---
--- Indexes for table `killinfo`
---
-ALTER TABLE `killinfo`
-  ADD PRIMARY KEY (`killid`),
-  ADD UNIQUE KEY `userid` (`userid`);
 
 --
 -- Indexes for table `users`
@@ -178,73 +56,15 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`userid`);
 
 --
--- Indexes for table `vote`
---
-ALTER TABLE `vote`
-  ADD PRIMARY KEY (`voteid`),
-  ADD KEY `comment` (`commentid`),
-  ADD KEY `cityid` (`cityid`),
-  ADD KEY `userid` (`userid`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `cities`
---
-ALTER TABLE `cities`
-  MODIFY `cityid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `commentid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `killinfo`
---
-ALTER TABLE `killinfo`
-  MODIFY `killid` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
---
--- AUTO_INCREMENT for table `vote`
---
-ALTER TABLE `vote`
-  MODIFY `voteid` int(11) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `attractions`
---
-ALTER TABLE `attractions`
-  ADD CONSTRAINT `attractions_ibfk_1` FOREIGN KEY (`cityid`) REFERENCES `cities` (`cityid`);
-
---
--- Constraints for table `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `city` FOREIGN KEY (`cityid`) REFERENCES `cities` (`cityid`),
-  ADD CONSTRAINT `user` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
-
---
--- Constraints for table `killinfo`
---
-ALTER TABLE `killinfo`
-  ADD CONSTRAINT `user_kill` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
-
---
--- Constraints for table `vote`
---
-ALTER TABLE `vote`
-  ADD CONSTRAINT `city_id` FOREIGN KEY (`cityid`) REFERENCES `cities` (`cityid`),
-  ADD CONSTRAINT `comment` FOREIGN KEY (`commentid`) REFERENCES `comments` (`commentid`),
-  ADD CONSTRAINT `user_id` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
